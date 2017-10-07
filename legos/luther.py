dd35c01924a565dbe495e1a64504cd929b34e209
Drew Pearce <drew@caffdev.com>
2017-10-07 00:06:16 -0400

    cleanup!
    + travis

--- legos/luther.py
+++ legos/luther.py
@@ -1,9 +1,9 @@
 from Legobot.Lego import Lego
 import requests
 import logging
-import json
 
 logger = logging.getLogger(__name__)
+
 
 class Luther(Lego):
     def listening_for(self, message):
@@ -14,13 +14,19 @@ class Luther(Lego):
                 logger.error('''Luther lego failed to check message text:
                             {}'''.format(e))
                 return False
+
     def handle(self, message):
         logger.debug('Handling message...')
         opts = self._handle_opts(message)
         # Set a default return_val in case we can't handle our crap
         return_val = '¯\_(ツ)_/¯'
         random_insult = self._get_insult()
-        return_val = random_insult
+        if random_insult is not None:
+            if len(message['text'].split()) > 1:
+                logger.info('Building addressed reply.')
+                return_val = self._build_reply(message['text'], random_insult)
+            else:
+                return_val = random_insult
 
         self.reply(message, return_val, opts)
 
@@ -35,15 +41,25 @@ class Luther(Lego):
         return opts
 
     def _get_insult(self):
-        insult_request = requests.get('https://api.drewpearce.tech/api/v2/random_luther?api_key=d8209ab55fbafeb4e13d4bde17532613d04ce119601077ae931f96df6e84653b')
+        df_url = 'https://api.drewpearce.tech/api/v2/random_luther'
+        K = 'd8209ab55fbafeb4e13d4bde17532613d04ce119601077ae931f96df6e84653b'
+        request_url = '{}?api_key={}'.format(df_url, K)
+        insult_request = requests.get(request_url)
         if insult_request.status_code == requests.codes.ok:
             insult = insult_request.text
         else:
             insult = 'There was an error retrieving your insult.'
         return insult
 
+    def _build_reply(self, message_text, insult):
+        addressee = message_text.split(' ', 1)[1]
+        logger.info('ADDRESSEE: {}'.format(addressee))
+        reply = '{}, {}'.format(addressee, insult)
+        return reply
+
     def get_name(self):
         return 'luther'
 
     def get_help(self):
-        return 'Fetch a Martin Luther insult. Usage: !luther [username|some noun]'
+        return '''Fetch a Martin Luther insult. Usage: !luther
+                [username|some noun]'''
